af92907fcb0b2a03ef0e1c84db00663d9958d253
jorgegus100 <jalameda100@gmail.com>
2014-08-28 19:49:31 -0400

    añadir permisos en controladores

--- protected/controllers/TutorController.php
+++ protected/controllers/TutorController.php
@@ -31,10 +31,10 @@ class TutorController extends Controller
 				'actions'=>array('index','view'),
 				'users'=>array('*'),
 			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                'actions'=>array('create','update','admin','delete'),
+                'users'=>array('@'),
+            ),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
 				'users'=>array('admin'),
